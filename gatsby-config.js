c36583d7ceb213ff4172c4da48bac92bf0b42b62
Robespra <rpages07@gmail.com>
2018-10-30 14:30:58 +0100

    added analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,18 @@ module.exports = {
   siteMetadata: {
     title: 'uxpages',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    // Shortcut for adding plugins without options.
+    'gatsby-plugin-react-helmet',
+    {
+      // Standard plugin with options example
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-38305116-1",
+        head: true,
+      },
+    },
+  
+  ],
   pathPrefix: "/uxpages",
 }
