bce673306a0334d2dbabc28f8e4d4f00a8f8545e
Robespra <rpages07@gmail.com>
2018-11-06 17:24:37 +0100

    fixed layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,54 +5,52 @@ let pathPrefix = `/`
 
 
 
-
-
-
-
 const IndexPage = () => (
+<div>
 
-<div className="row align-center hero_block">
 
 
+
+<div className="row align-middle hero_block">
 	<div className="small-11 medium-6 columns hero_pctre">
 		<img src={require('../images/z3.jpg')} alt="pages charly espra jeremy robertttt aqui está?"/>
 	</div>
 	<div className="small-11 medium-6 columns">
 	    <h1 className="hidden-h1">uxpages ux pages</h1>
 	    <h2 className="hero_line">I design digital products from ux strategy to implementation</h2>
+    </div>
 </div>
-<div className="row prjct-dtls">
 
 
 <div id="recent_work" className="row align-center">
-<div className="small-9 medium-11 columns last-prjct padding5_both">
-<div className="row prjct-dtls">
-	<div className="small-11 columns dtls-text">
-		<h3>Helping companies to convert more with convy</h3>
-		<div className="skills_underline_orange"></div>
-	</div>
-	<div className="small-11 medium-4 columns dtls-text">
-	<p>Conversion Optimization Design</p>
-	<p>you may be interested in :</p>
-	<ul className="no_bullet">
-		<li className="checked">UX Design Consultancy</li>
-		<li className="checked">Design Optimization</li>
-		<li className="checked">Analytics</li>
-		<li className="checked">a/b Testing</li>
-	</ul>
-	</div>
-	<div className="small-11 medium-8 columns prjct-img_block_pstn-relative_opposite">
-		<div className="prjct-img_block_opposite">
-			<div className="prjct-img_block_inner_pstn-relative_opposite">
-			<img className="prjct-large-bg lazy" src={require('../images/prjctimg3_convy.jpg')} alt="convy"/>
-				<Link className="project__item-btn" to="../convy" onclick="track('Click-see-project-convy','Click-see-project-convy')"><h5 className="gradient_effect">see case study</h5></Link>
-			</div>
-		</div>
-	</div>
-</div>
-
-
-<div id="recent_work" className="row prjct-dtls ">
+    <div className="small-9 medium-11 columns last-prjct padding5_both">
+        <div className="row prjct-dtls">
+            <div className="small-11 columns dtls-text">
+                <h3>Helping companies to convert more with convy</h3>
+                <div className="skills_underline_orange"></div>
+            </div>
+            <div className="small-11 medium-4 columns dtls-text">
+            <p>Conversion Optimization Design</p>
+            <p>you may be interested in :</p>
+            <ul className="no_bullet">
+                <li className="checked">UX Design Consultancy</li>
+                <li className="checked">Design Optimization</li>
+                <li className="checked">Analytics</li>
+                <li className="checked">a/b Testing</li>
+            </ul>
+            </div>
+            <div className="small-11 medium-8 columns prjct-img_block_pstn-relative_opposite">
+                <div className="prjct-img_block_opposite">
+                    <div className="prjct-img_block_inner_pstn-relative_opposite">
+                    <img className="prjct-large-bg lazy" src={require('../images/prjctimg3_convy.jpg')} alt="convy"/>
+                        <Link className="project__item-btn" to="../convy" onclick="track('Click-see-project-convy','Click-see-project-convy')"><h5 className="gradient_effect">see case study</h5></Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className="small-9 medium-11 columns last-prjct padding5_both">
+        <div className="row prjct-dtls">
             <div className="small-11 columns dtls-text">
                 <h3>Designing a chatbot and a dashboard for mēkācreative.io</h3>
                 <div className="skills_underline_orange"></div>
@@ -61,30 +59,31 @@ const IndexPage = () => (
                         <p>(in progress)</p>
                         <p>Mixing psychology, machine learning and conversational ui.</p>
                         <p>you may be interested in : </p>
-                   <ul className="no_bullet">
+                    <ul className="no_bullet">
                             <li className="checked">Conversational design</li>
                             <li className="checked">Gamification</li>
                             <li className="checked">Product strategy</li>
                             </ul>
                 </div>
-                 <div className="small-11 medium-8 columns prjct-img_block_pstn-relative_opposite">
+                    <div className="small-11 medium-8 columns prjct-img_block_pstn-relative_opposite">
                     <div className="prjct-img_block_opposite">
                         <div className="prjct-img_block_inner_pstn-relative_opposite">
                             <video className="prjct-large-bg lazy" width="100%" height="auto" autoplay="autoplay" loop>
-<source src={require('../images/meka/interaction_meka.mp4')}/></video>
+                            <source src={require('../images/meka/interaction_meka.mp4')}/></video>
                             <Link className="project__item-btn" to="../meka" onclick="track('Click-see-project-meka','Click-see-project-meka')"><h5 className="gradient_effect">see case study</h5></Link>  
                         </div>
                     </div>
                 </div>
-            </div>
+        </div>        
+    </div>
 
-
- <div id="recent_work" className="row prjct-dtls ">
+    <div className="small-9 medium-11 columns last-prjct padding5_both">
+        <div className="row prjct-dtls ">
             <div className="small-11 columns dtls-text">
                 <h3>Multiple products development with kelisto.es</h3>
                 <div className="skills_underline_orange"></div>
                 </div>
-               <div className="small-11 medium-4 columns dtls-text">
+            <div className="small-11 medium-4 columns dtls-text">
                     
                     <p>[SPANISH] 2 years project. Finance, Insurance, Comms & Energy Price comparators. Ecommerce & content.</p>
                     <p>you may be interested in : </p>
@@ -96,60 +95,59 @@ const IndexPage = () => (
                             <li className="checked twolines">Conversion-optimization design</li>
                             </ul>
                 </div>
-                 <div className="small-11 medium-8 columns prjct-img_block_pstn-relative_opposite">
+                <div className="small-11 medium-8 columns prjct-img_block_pstn-relative_opposite">
                     <div className="prjct-img_block_opposite">
                         <div className="prjct-img_block_inner_pstn-relative_opposite">
-                        	<img className="prjct-large-bg lazy" src={require('../images/prjctimg3_kelisto.jpg')} alt="kelisto hero case study"/>
+                            <img className="prjct-large-bg lazy" src={require('../images/prjctimg3_kelisto.jpg')} alt="kelisto hero case study"/>
                             <div className="prjct-img_block_inner_pstn-absolute_opposite">
-                               
-                   
+                            
+                
                             </div>
                             <img src={require('../images/kelisto/numbers_kelisto.svg')} alt="kelisto hero case study"/>
                             <Link className="project__item-btn" to="../kelisto" onclick="track('Click-see-project-kelisto','Click-see-project-kelisto')"><h5 className="gradient_effect">see case study</h5></Link>  
                         </div>
                     </div>
-                </div>
             </div>
-            <div id="recent_work" className="row no-margin">
+        </div>
+    </div>
+    <div className="small-9 medium-11 columns last-prjct padding5_both">
+        <div className="row last-prjct-dtls ">
             <div className="small-11 columns dtls-text">
                 <h3>Starting from scratch with sharemos.com</h3>
                 <div className="skills_underline_orange"></div>
+            </div>
+            <div className="small-11 medium-4 columns dtls-text">
+                <div className="dtls-text-p">
+                <p>[ENG / SPANISH - project in progress] Two-side marketplace.
+                    Rent whatever you need from people like you. Wherever.</p>
+                    <p>you may be interested in :</p> 
+                    <ul className="no_bullet">
+                        <li className="checked">MVP</li>
+                        <li className="checked">Branding/visual design</li>
+                        <li className="checked">UX UI</li>
+                        <li className="checked">Front-end code</li>
+                        <li className="checked">Android App design</li>
+                        </ul>
                 </div>
-               <div className="small-11 medium-4 columns dtls-text">
-                    <div className="dtls-text-p">
-                    <p>[ENG / SPANISH - project in progress] Two-side marketplace.
-                        Rent whatever you need from people like you. Wherever.</p>
-                        <p>you may be interested in :</p> 
-                        <ul className="no_bullet">
-                            <li className="checked">MVP</li>
-                            <li className="checked">Branding/visual design</li>
-                            <li className="checked">UX UI</li>
-                            <li className="checked">Front-end code</li>
-                            <li className="checked">Android App design</li>
-                            </ul>
-                    </div>
-                </div>
-                 <div className="small-11 medium-8 columns prjct-img_block_pstn-relative_opposite">
-                    <div className="prjct-img_block_opposite">
-                        <div className="prjct-img_block_inner_pstn-relative_opposite">
-                        	<img className="prjct-large-bg lazy" src={require('../images/prjctimg3_sharemos.jpg')} alt="kelisto hero case study"/>
-                            <div className="prjct-img_block_inner_pstn-absolute_opposite">
-                               
-                            </div>
-                            <Link className="project__item-btn" to="../sharemos" onclick="track('Click-see-project-sharemos','Click-see-project-sharemos')"><h5 className="gradient_effect">see case study</h5></Link> 
+            </div>
+            <div className="small-11 medium-8 columns prjct-img_block_pstn-relative_opposite">
+                <div className="prjct-img_block_opposite">
+                    <div className="prjct-img_block_inner_pstn-relative_opposite">
+                        <img className="prjct-large-bg lazy" src={require('../images/prjctimg3_sharemos.jpg')} alt="kelisto hero case study"/>
+                        <div className="prjct-img_block_inner_pstn-absolute_opposite">
+                            
                         </div>
+                        <Link className="project__item-btn" to="../sharemos" onclick="track('Click-see-project-sharemos','Click-see-project-sharemos')"><h5 className="gradient_effect">see case study</h5></Link> 
                     </div>
                 </div>
             </div>
-
-
-</div>
+        </div>
+    </div>
 </div>
 
 <div id="skills" className="row align-center padding5_top">
     <div className="small-9 medium-11 columns">
         <h2>how can I help you ?</h2>
-
         <h3>I started designing <span className="skills_highlight">user interface </span>in 1999 and have progressively expanded my skillset to <span className="skills_highlight">strategic product-oriented UX design.</span> Product design is not an art guided solely by creativity and intuition : I do believe in <span className="skills_highlight">lean customer development and metrics.</span>
         </h3>
         <div className="row align-left">
@@ -255,10 +253,7 @@ const IndexPage = () => (
 
         </div>
     </div>
-    </div>
-   
-    </div>
-
+</div>
 
 
 
@@ -290,6 +285,9 @@ const IndexPage = () => (
 		</div>
 	</div>
 </div>
+</div>
+
+
 
 
 <div id="fourth" className="row align-center padding5_both">
@@ -325,7 +323,6 @@ const IndexPage = () => (
 		</div>
 	</div>
 </div>
-
 
 
 
@@ -373,8 +370,7 @@ const IndexPage = () => (
 
 
 
-
-    <footer>
+<footer>
     <div className="row large-unstack align-center">
       <div className="column">
         <small>&copy; 2018 uxpages eirl</small>
@@ -382,11 +378,15 @@ const IndexPage = () => (
     </div>
 </footer>
 
+
+
+
+
+
+
+
+
 </div>
-</div>
-)
 
-
-
-
+) 
 export default IndexPage
