adc18c34dfcba8f1420dbebcdcd2c50bba360552
Robespra <rpages07@gmail.com>
2018-11-16 13:41:31 +0100

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+    <div className="row align-middle hero_block">
+	<div className="small-11 medium-6 columns">
+  <h1>woops, no page found....</h1>
+	</div>
+	<div className="small-11 medium-6 columns">
+  <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
+    </div>
+    </div>
 )
 
 export default NotFoundPage
