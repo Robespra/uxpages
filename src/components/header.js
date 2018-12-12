af3a21e3d33ab61800ad194727353fb4f5bb1c5e
Robespra <rpages07@gmail.com>
2018-12-12 15:06:41 +0100

    header update

--- src/components/header.js
+++ src/components/header.js
@@ -37,7 +37,7 @@ handleScroll = (event) => {
 
 
   <div className="availability">
-    <h6>I am available for product design projects starting January 2019. Interested? 
+    <h6>I am available for product design projects starting April 2019. Interested? 
     <Link className="fat_underline" to="../#contact-form" onClick="track('Click-contact-project-topbar-index','Click-contact-project-topbar-index')">  contact me</Link></h6>
   </div>
   <nav className="sidebar-nav">
