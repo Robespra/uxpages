fc683f643afb121d8ec7b90d82e37622cff91ecb
Robespra <rpages07@gmail.com>
2019-01-29 09:41:09 +0100

    let pathPrefix addition2

--- src/pages/convy.js
+++ src/pages/convy.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import { Link } from "gatsby"
-
+let pathPrefix = `/`
 
 const Convy = () => (
 <div>
