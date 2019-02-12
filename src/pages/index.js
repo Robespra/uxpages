eaf184c1ce653435dab950e684849ede3f49ebae
Robespra <rpages07@gmail.com>
2019-02-12 16:57:16 -0500

    changed published dates

--- src/pages/index.js
+++ src/pages/index.js
@@ -305,22 +305,22 @@ const IndexPage = () => (
 			</div>
 			<div className="small-11 medium-8 columns">
 				<div className="callout_publications">
-					<small> 23/04/2018</small>
+					<small> 23/02/2019</small>
 					<a href="http://convy.io/read/articles/good_emails" target="_blank" rel="noopener" onclick="track('Click-see-article-solitude','Click-see-article-solitude')"><h3>5 Tips To Make Emails That Convert<span className="target_blank"></span></h3></a>
 					<p>Email design is hard. In this article we will review 5 rules that unsure email good conversion.</p>
 				</div>
 				<div className="callout_publications">
-					<small> 03/02/2018</small>
+					<small> 03/01/2019</small>
 					<a href="http://www.mekacreative.io/blog/articles/the_five_paradoxes_of_creativity" target="_blank" rel="noopener" onclick="track('Click-see-article-five','Click-see-article-five')"><h3>The five paradoxes of creativity <span className="target_blank"></span></h3></a>
 					<p>Understanding creativity means discerning the different paradoxes that compose it.</p>
 				</div>
 				<div className="callout_publications">
-					<small> 23/12/2017</small>
+					<small> 23/12/2018</small>
 					<a href="http://convy.io/read/articles/attractive_digital" target="_blank" rel="noopener" onclick="track('Click-see-article-five','Click-see-article-five')"><h3>5 Pro Tips To Make Your Product More Attractive<span className="target_blank"></span></h3></a>
 					<p>Reaction to digital product appearances are fast, unconscious and will affect the way users engage with it. In this article we will share ways to ensure your product provides a great first impression.</p>
 				</div>
 				<div className="callout_publications">
-					<small> 11/12/2017</small>
+					<small> 11/12/2018</small>
 					<a href="http://www.mekacreative.io/blog/articles/empathy_and_creativity" target="_blank" rel="noopener" onclick="track('Click-see-article-empathy','Click-see-article-empathy')"><h3>Empathy and creativity<span className="target_blank"></span></h3></a>
 					<p>This phenomenon is the basis of human links and artistic communication.</p>
 				</div>
